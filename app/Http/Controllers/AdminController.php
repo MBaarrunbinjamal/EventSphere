350ab9ca24946ddded39db60d626fff9633804d1
Meer <muhammedmeer298@gmail.com>
2025-09-10 20:54:34 +0500

    jhf

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\AdminController;
+
+use App\Exports\UsersExport;
+use Maatwebsite\Excel\Facades\Excel;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class AdminController extends Controller
 {
-    //
+    public function getuser()
+    {
+       $rec = user::get();
+        return view('Admin.user',compact('rec'));
+    }
+
+ public function exportExcel()
+    {
+        return Excel::download(new UsersExport, 'users.xlsx');
+    }
+
+     public function exportPDF()
+    {
+        $users = User::select('id','name','email','role')->get();
+        $pdf = Pdf::loadView('Admin.users-pdf', compact('users'));
+        return $pdf->download('users.pdf');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Middleware\Adminmiddleware;
 use App\Http\Middleware\Studentmiddleware;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('clients.index');
@@ -14,9 +16,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    
     Route::get('/mdetails', function () {
     return view('clients.meeting-details');
 });
@@ -43,3 +43,9 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), Studentmidd
 Route::get('/dash', function () {
     return view('Admin.dashboard');
 });
+Route::get('/user', [AdminController::class,('getuser')]);
+
+
+Route::get('/export-users-excel', [AdminController::class, 'exportExcel'])->name('users.export.excel');
+Route::get('/export-users-pdf', [AdminController::class, 'exportPDF'])->name('users.export.pdf');
+

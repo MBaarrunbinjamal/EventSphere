8e46a26cab5d4e76d2656b347de6e34783546345
MBaarrunbinjamal <mbaarrun2008@gmail.com~>
2025-09-10 12:30:04 +0500

    sammadua the chocolaty bitch

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Middleware\Adminmiddleware;
+use App\Http\Middleware\Studentmiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('clients.index');
 });
-Route::get('/mdetails', function () {
-    return view('clients.meeting-details');
-});
-Route::get('/meetingss', function () {
-    return view('clients.meetings');
-});
-
-Route::post('/contactform', [App\Http\Controllers\HomeController::class,('insertcontact')])->name('contactform');
 
 Route::middleware([
     'auth:sanctum',
@@ -22,4 +17,26 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('/mdetails', function () {
+    return view('clients.meeting-details');
 });
+Route::get('/meetingss', function () {
+    return view('clients.meetings');
+});
+
+Route::post('/contactform', [HomeController::class,('insertcontact')])->name('contactform');
+
+});
+// Admin middleware start
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), Adminmiddleware::class])->group(function () {
+
+
+});
+// Admin middleware end
+ 
+// Student middleware start
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), Studentmiddleware::class])->group(function () {
+
+
+});
+// Student middleware end
